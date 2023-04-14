85579cd65e0301c4712fd1f7f9465970f23a0eb1
RonalShemO <shemobaigwa79@gmail.com>
2023-04-14 12:56:08 +0300

    first commit

---
+++ app/src/main/java/com/example/shemsfirebasedatabase/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.shemsfirebasedatabase
+
+import android.annotation.SuppressLint
+import android.app.ProgressDialog
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+    lateinit var edtName:EditText
+    lateinit var edtEmail:EditText
+    lateinit var edtIdNumber:EditText
+    lateinit var btnSave:Button
+    lateinit var btnView:Button
+    lateinit var progressDialog: ProgressDialog
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        edtName = findViewById(R.id.mEdtName)
+        edtEmail = findViewById(R.id.mEdtEmail)
+        edtIdNumber = findViewById(R.id.mEdtIdNumber)
+        btnSave = findViewById(R.id.mBtnSave)
+        btnView= findViewById(R.id.mBtnView)
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Saving")
+        progressDialog.setMessage("Please wait...")
+        btnSave.setOnClickListener {
+
+        }
+        btnView.setOnClickListener {
+
+        }
+    }
+}
